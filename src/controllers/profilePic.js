d64c6372b963ea51e42c25ad51e544145b2756ba
Shukurah <shukkike@gmail.com>
2023-04-08 14:50:27 +0100

    Upload profile pic

--- src/controllers/profilePic.js
+++ src/controllers/profilePic.js
@@ -2,6 +2,7 @@ import { dataUri, uploadToCloud } from '../utils/upload';
 import { userModel } from './auth/userAuth';
 
 export const uploadProfilePic = async (req, res) => {
+  const token = req.token;
   try {
     const { userId } = req.user.userInfo;
 
@@ -11,8 +12,30 @@ export const uploadProfilePic = async (req, res) => {
       const data = { profile_pic: imageUrl };
       const clause = `WHERE user_details_id = '${userId}'`;
       const addProfilePic = await userModel.editFromTable(data, clause);
+      const info = `first_name, last_name, password, email, user_type, phone_number`;
+      const infoClause = `WHERE user_details_id = '${userId}'`;
+      const infoData = await userModel.select(info, infoClause);
+      const {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        user_type: userType,
+        phone_number: phoneNumber,
+        profile_pic: profilePic,
+      } = infoData.rows[0];
+      const userInfo = {
+        userId,
+        firstName,
+        lastName,
+        email,
+        userType,
+        phoneNumber,
+        profilePic,
+      };
       return res.status(200).json({
         message: 'File successfully uploaded',
+        userInfo,
+        token,
         success: true,
       });
     } else {
