8f7fb9904fba8edf72b7003537bcf1246bc51d1d
Shukurah <shukkike@gmail.com>
2023-04-08 08:30:11 +0100

    Fix cors

--- src/app.js
+++ src/app.js
@@ -22,12 +22,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-);
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
